c4bd2ced56a87338b8af6f7b755421c594fac797
Yulia Sosnina <yuliia.sosnina@ktech.com>
2024-02-13 16:48:09 +0000

    2 controlelrds

--- src/main/java/org/example/api/ComplimentController.java
+++ src/main/java/org/example/api/ComplimentController.java
@@ -1,19 +1,11 @@
 package org.example.api;
 
-import main.java.org.example.ComplimentPicker;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @Controller
 public class ComplimentController {
-
-  @GetMapping("/random-compliment")
-  @ResponseBody
-  public String getRandomCompliment() {
-    return ComplimentPicker.getRandomCompliment();
-  }
 
   @GetMapping("")
   public String returnIndex() {
