45ad0b0a00c746734e4329d02f0e834542ce10d5
Yulia Sosnina <yuliia.sosnina@ktech.com>
2024-03-05 21:54:19 +0000

    added compl

--- src/main/java/org/example/ComplimentDatabase.java
+++ src/main/java/org/example/ComplimentDatabase.java
@@ -11,6 +11,46 @@ public class ComplimentDatabase {
     compliments = new ArrayList<>();
     compliments.add(Compliments.c1);
     compliments.add(Compliments.c2);
+    compliments.add(Compliments.c3);
+    compliments.add(Compliments.c4);
+    compliments.add(Compliments.c5);
+    compliments.add(Compliments.c6);
+    compliments.add(Compliments.c7);
+    compliments.add(Compliments.c8);
+    compliments.add(Compliments.c9);
+    compliments.add(Compliments.c10);
+    compliments.add(Compliments.c11);
+    compliments.add(Compliments.c12);
+    compliments.add(Compliments.c13);
+    compliments.add(Compliments.c14);
+    compliments.add(Compliments.c15);
+    compliments.add(Compliments.c16);
+    compliments.add(Compliments.c17);
+    compliments.add(Compliments.c18);
+    compliments.add(Compliments.c19);
+    compliments.add(Compliments.c20);
+    compliments.add(Compliments.c21);
+    compliments.add(Compliments.c22);
+    compliments.add(Compliments.c23);
+    compliments.add(Compliments.c24);
+    compliments.add(Compliments.c25);
+    compliments.add(Compliments.c26);
+    compliments.add(Compliments.c27);
+    compliments.add(Compliments.c28);
+    compliments.add(Compliments.c29);
+    compliments.add(Compliments.c30);
+    compliments.add(Compliments.c31);
+    compliments.add(Compliments.c32);
+    compliments.add(Compliments.c33);
+    compliments.add(Compliments.c34);
+    compliments.add(Compliments.c35);
+    compliments.add(Compliments.c36);
+    compliments.add(Compliments.c37);
+    compliments.add(Compliments.c38);
+    compliments.add(Compliments.c39);
+    compliments.add(Compliments.c40);
+    compliments.add(Compliments.c41);
+    compliments.add(Compliments.c42);
   }
 
   public List<String> getCompliments() {
--- src/main/java/org/example/Compliments.java
+++ src/main/java/org/example/Compliments.java
@@ -2,6 +2,47 @@ package org.example;
 
 public class Compliments {
 
-  public static final String c1 = "compliment 1";
-  public static final String c2 = "compliment 2";
+  public static final String c1 = "I do like your brain a lot";
+  public static final String c2 = "Your critical thinking is bewitching";
+  public static final String c3 = "It's a pleasure to lie on your chest. And shoulders";
+  public static final String c4 = "Your puppy is the cutest. Just like you";
+  public static final String c5 = "I love your sense of explore and passion to learn something new";
+  public static final String c6 = "I love your strong arms, they make me feel amazing when you hug me from behind";
+  public static final String c7 = "I feel so peaceful when sleeping next to you";
+  public static final String c8 = "I do love your style. When your wear shirts, it's just oh my god";
+  public static final String c9 = "I love your playfulness. It resonates with mine so well";
+  public static final String c10 = "Your craziness appeals to me a lot";
+  public static final String c11 = "You have such a kind heart, I could clearly see it the way you treat animals. And me when I'm sick";
+  public static final String c12 = "It's so easy for me to be myself with you. Thank you for being you";
+  public static final String c13 = "It's so nice to see your drive and passion to what you do, I was fascinated by what you taught me about lighting and design";
+  public static final String c14 = "Your ambitions are beautiful";
+  public static final String c15 = "I love your gentleman'ish gestures. They make it so easy for me just to be a woman with you";
+  public static final String c16 = "You are very, very, very cute (in the sense I mean it)";
+  public static final String c17 = "I love the way you give me compliments. They are always well-thought and special. Never generic";
+  public static final String c18 = "I love to be lying on your shoulder when listening to songs from Twin Peaks in your car";
+  public static final String c19 = "You are so handsome, it's irresistible";
+  public static final String c20 = "Your taste is amazing, so subtle and exquisite. I loved all the songs you shared with me";
+  public static final String c21= "I love your sense of aesthetics";
+  public static final String c22= "I love how you surround yourself with special meanings, like those pictures around your house which have a story behind them";
+  public static final String c23= "Your creativity is mind-blowing, it ignites my internal fire a lot";
+  public static final String c24= "I love discussing and implementing ideas with you, even when it's something silly";
+  public static final String c25= "You make me laugh a lot, it's always so much fun to spend time with you";
+  public static final String c26= "I started to smile a lot more since I met you, thank you";
+  public static final String c27= "I love your dedication and enthusiasm, they are beautiful";
+  public static final String c28= "You are so damn sexy";
+  public static final String c29= "I love that you're able to see so many things about me which others fail to see. This matters a lot to me";
+  public static final String c30= "Your sense of adventure is contagious";
+  public static final String c31= "You somehow manage to keep me interested non-stop, I enjoy learning something new from you";
+  public static final String c32= "I did not realise I could have so much fun at a train station date! It's fun to spend time with you even in silly places at the most inappropriate moments";
+  public static final String c33= "I love your spontaneous creativity, like with those lamps with an in-built microphone. It was such a beautiful idea";
+  public static final String c34= "I like when you call me a good girl";
+  public static final String c35= "Your confidence is super attractive, especially when you say 'end of story'";
+  public static final String c36= "You leave such lovely marks on my bum, love them!";
+  public static final String c37= "I appreciate that you don't judge all of my crazy ideas";
+  public static final String c38= "Your thoughts are so expressive, it makes it easy to communicate with you";
+  public static final String c39= "You are so kind to me";
+  public static final String c40= "I think the crown suits you ;)";
+  public static final String c41= "It feels nice to be your Kitty";
+  public static final String c42= "Distracting you from driving is my absolute pleasure";
+
 }
